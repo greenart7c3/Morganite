540f71d04728f0cbf0a285ce9bfb4f9ffd4315c9
greenart7c3 <greenart7c3@proton.me>
2026-02-04 09:26:03 -0300

    Setup git hooks

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,15 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
+
+tasks.register<Copy>("installGitHook") {
+    from(File(rootProject.rootDir, "git-hooks/pre-commit"))
+    from(File(rootProject.rootDir, "git-hooks/pre-push"))
+    into(File(rootProject.rootDir, ".git/hooks"))
+    filePermissions {
+        unix(0b111101101)
+    }
+}
+
+
+tasks.getByPath(":app:preBuild").dependsOn(tasks.getByName("installGitHook"))
